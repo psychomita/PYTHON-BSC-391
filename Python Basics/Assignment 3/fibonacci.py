9fd28ad74cd9e0286db6e43aef65c2b1f40d13f8
Suchismita Bose <suchismitabose29@gmail.com>
2023-09-24 15:22:31 +0530

    Add files via upload

---
+++ Python Basics/Assignment 3/fibonacci.py
@@ -0,0 +1,12 @@
+def fibo (n) :
+    a=0
+    b=1
+    print(a)
+    print(b)
+    for x in range (n-1) :
+        c=a+b
+        print(c)
+        a=b
+        b=c
+n=int(input("Enter the number of terms in Fibonacci sequence : "))
+fibo(n)
