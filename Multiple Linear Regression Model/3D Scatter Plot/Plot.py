d8fd773e9517fb945a4c35ddd57aec1cbc7ab87c
Suchismita Bose <suchismitabose29@gmail.com>
2023-10-11 02:31:53 +0530

    Update Plot.py

--- Multiple Linear Regression Model/3D Scatter Plot/Plot.py
+++ Multiple Linear Regression Model/3D Scatter Plot/Plot.py
@@ -1,21 +1,27 @@
-import numpy as np 
-import matplotlib.pyplot as plt 
-from mpl_toolkits.mplot3d import Axes3D 
-# Given data 
-Y = np.array([293, 230, 172, 91, 113, 125]) 
-x1 = np.array([1.6, 15.5, 22.0, 43.0, 33.0, 40.0]) 
-x2 = np.array([851, 816, 1058, 1201, 1357, 1115]) 
-# Create a 3D scatter plot 
-fig = plt.figure() 
-ax = fig.add_subplot(111, projection='3d') 
-# Plot the data points 
-ax.scatter(x1, x2, Y, c='b', marker='o', label='Data Points') 
-# Set labels for the axes 
-ax.set_xlabel('Oil Viscosity (x1)') 
-ax.set_ylabel('Load (x2)') 
-ax.set_zlabel('Bearing Wear (Y)') 
-# Set title and legend 
-plt.title('Multiple Linear Regression - 3D Scatter Plot') 
-ax.legend() 
-# Show the plot 
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+#Feeding data from .csv file
+df=pd.read_csv(r"C:\Users\suchi\OneDrive\Desktop\Plot\table.csv")
+print(df)
+
+#Creating a 3D Scatter Plot
+fig= plt.figure()
+ax=fig.add_subplot(111,projection='3d')
+
+#Marking axes
+x=df['x1'].values
+y=df['x2'].values
+z=df['Y'].values
+a=ax.set_xlabel('Oil Viscocity (x1)',linespacing=3.2)
+b=ax.set_ylabel('Load (x2)',linespacing=3.1)
+d=ax.set_zlabel('Bearing of Wear (Y)',linespacing=3.1)
+ax.scatter(x, y, z, c='b', marker='o', label='Data Points')
+
+#Setting title and legend
+ax.set_title('Multiple Linear Regression- 3D Scatter Plot')
+ax.legend()
+
+#Showing the plot
 plt.show()
